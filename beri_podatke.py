e40d1945d290af3080c04f131c45fad71825128e
Leon <leon.premky@gmail.com>
2019-03-29 16:05:28 +0100

    dodatek

--- beri_podatke.py
+++ beri_podatke.py
@@ -1,11 +1,8 @@
 import pandas
 import datetime
 
-'''dtype=[('date', datetime.datetime), ('dayOfWeek', int), ('timeFrame', int), ('userID', int), ('siteID', int), ('campaignID', int), ('adID', int), ('zoneID', int), ('masterSiteID', int), ('siteCategory', int), ('adIndustry', int), ('requests', int), ('views', int), ('clicks', int)]'''
-
-
 def read_data(t, file):
-    return pandas.read_csv(file, delimiter='\t' ).values[:, t]
+    return pandas.read_csv(file, delimiter='\t', usecols=t).values
 
 
 
---
+++ campaign_clicks.py
@@ -0,0 +1,45 @@
+from beri_podatke import read_data as read
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+
+path = 'C:/Users/Leon/Documents/iProm_podatki/'
+
+
+files = []
+
+for r, d, f in os.walk(path):
+    for file in f:
+        if '.csv' in file:
+            files.append(os.path.join(r, file))
+
+data = read([5, 13], files[0])
+data = np.array([[x, y] for [x, y] in data[:, [0, 1]] if x > 0 and y > 0])
+i = 1
+print(i)
+
+for file in files[1:]:
+    temp = read([5, 13], file)
+    temp = np.array([[x, y] for [x, y] in temp[:, [0, 1]] if x > 0 and y > 0])
+    data = np.concatenate((data, temp))
+    i += 1
+    print(i)
+
+print(data)
+
+df = pd.DataFrame(data, columns=['kamp', 'ocena'])
+df = df.groupby(['kamp']).sum().reset_index()
+df = df.sort_values(by=['ocena'], ascending=False)
+df.describe
+
+data = df.to_numpy()
+
+
+indexes = np.arange(len(data[:, 0]))
+print(data)
+
+plt.bar(indexes, data[:, 1])
+plt.xlabel("ID_kampanje")
+plt.ylabel('stevilo_klikov')
+plt.show()
