e9196773a4c2eccc45fae6e1c3c7c9c3756c7d4f
DomenRok <bruncekdomenrok@gmail.com>
2019-04-01 16:29:37 +0200

    project refractor, added graph ctr_dnevi

---
+++ ctr_category.py
@@ -0,0 +1,31 @@
+import pandas as pd, numpy as np
+import os, glob
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+
+path = r"C:\Users\Domen Brunček\Desktop\FRI\4 semester\Data Mining\Project\podatki"
+all_files = glob.glob(os.path.join(path, "*.csv"))
+df = pd.concat((pd.read_csv(f, header=None, sep='\t', usecols=[3, 9, 12]) for f in all_files), ignore_index=True)
+data = df.loc[(df[9] != 0) & (df[12] > 0)][[3,9]].groupby(9).mean().reset_index()
+
+sortedValues= data.sort_values(by=3).values
+_panoge = ["dom in vrt", "arts & entertainment", "nepremičnine",
+           "careers", "Mlade družine", "Novice",
+           "Sports", "law Govt & Politics", "Technology & Computing",
+           "Style & Fashion", "Travel","Business", "Kulinarika"]
+y_pos = np.arange(len(_panoge))
+
+
+print(sortedValues)
+
+fig, ax = plt.subplots()
+ax.barh(y_pos, sortedValues[:, 1], align='center',
+        color='green', ecolor='black')
+ax.set_yticks(y_pos)
+ax.set_yticklabels(_panoge)
+ax.invert_yaxis()  # labels read top-to-bottom
+ax.set_xlabel('CTR')
+ax.set_title('CTR per SiteCategory')
+plt.plot()
+plt.show()
---
+++ ctr_dnevi.py
@@ -0,0 +1,28 @@
+import pandas as pd, numpy as np
+import os, glob
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+path = r"C:\Users\Domen Brunček\Desktop\FRI\4 semester\Data Mining\Project\podatki"
+
+all_files = glob.glob(os.path.join(path, "*.csv"))
+df = pd.concat((pd.read_csv(f, header=None, sep='\t', usecols=[1,12,13]) for f in all_files), ignore_index=True)
+
+
+data = df.loc[df[12] > 0].groupby(1)[[1,13]].mean()
+print(data)
+
+dnevi = ['Ponedeljek', 'Torek', 'Sreda', 'Četrtek', 'Petek', 'Sobota', 'Nedelja']
+y_pos = np.arange(len(dnevi))
+clicks = data.values[:, 1]
+
+
+ax = plt.axes()
+ax.yaxis.set_major_formatter(ticker.PercentFormatter(1.0, decimals=2))
+
+
+plt.bar(y_pos, clicks, align='center', alpha=0.5)
+plt.xticks(y_pos, dnevi)
+plt.ylabel('CTR')
+
+plt.show()
