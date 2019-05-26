a3d8fcf5d933648f1ddd2fddad48d003134f6b9e
DomenRok <bruncekdomenrok@gmail.com>
2019-05-26 16:02:49 +0200

    begining working on matrix for testing

--- Clustering/kmeans_0.py
+++ Clustering/kmeans_0.py
@@ -10,18 +10,24 @@ PATH = r"C:\Users\Domen Brunček\Desktop\FRI\4 semester\Data Mining\Project\poda
 
 
 class ModelUsersWithNoClicks:
-    def __init__(self, path):
+    def __init__(self, path, k, plot=False):
         self.all_files = glob.glob(path)
-        self.df = self.process_data()
+        self.k = k
+        self.plot = plot
+        self.df, self.original_df = self.process_data()
+        self.categories = np.arange(38)
         self.matrix = self.make_matrix()
+        self.kmeans, self.labels, self.centroids, self.colors = self.k_means()
+        self.results()
 
     def process_data(self):
-        original_df = pd.concat((pd.read_csv(f, header=0, sep='\t', usecols=[3, 9], names=["UserID", "SiteCategory"], dtype={"UserID": np.int64, "siteCategory": np.float}) for f in self.all_files), ignore_index=True)
+        original_df = pd.concat((pd.read_csv(f, header=0, sep='\t', usecols=[3, 9], names=["UserID", "SiteCategory"],
+                                             dtype={"UserID": np.int64, "siteCategory": np.float}) for f in self.all_files), ignore_index=True)
         df = original_df.loc[(original_df["SiteCategory"] != 0)]
         df = df.groupby(["UserID", "SiteCategory"]).size()
         df = df.groupby(level=0).apply(lambda x: 100 * x / float(x.sum())).reset_index()
         df.rename(columns={df.columns[2]: "Size"}, inplace=True)
-        return df
+        return df, original_df
 
     def make_matrix(self):
         users = self.df["UserID"].unique()[:-1]  # vsi unikatni userji (no duplicates)
@@ -31,14 +37,7 @@ class ModelUsersWithNoClicks:
         # user = UserID in SiteCategory in Size
         for index, user in self.df.iterrows():
             vector_users[user["UserID"]][user["SiteCategory"]] = user["Size"]  # prvi slovar so userji, kljuci so slovarji kategorij vrednosr je size(procenti)
-
-        # fill matrix rows with 0
-        matrika = []
-        for _ in range(0, len(users)):
-            row = []
-            for _ in range(0, len(categories)):
-                row.append(0)
-            matrika.append(row)
+        matrika = np.zeros([len(users), len(categories)])
 
         # skozi vse userje (i = vrstica) in skozi vse kategorije (j = stolpec)
         for i, user in enumerate(users):
@@ -47,18 +46,37 @@ class ModelUsersWithNoClicks:
                     matrika[i][j] = vector_users[user][category]
         return np.array(matrika)
 
-    def kMeans(self, k, plot=False):
+    def k_means(self):
+        """ vrne """
         embedding = PCA(n_components=2)
-        dimension_reduce = embedding.fit_transform(self.matrix[:100])
-        print(dimension_reduce)
+        dimension_reduce = embedding.fit_transform(self.matrix[:500])
+        #print(dimension_reduce)
 
-        colors = ["#%06X" % randint(0, 0xFFFFFF) for i in range(k)]
-        kmeans = KMeans(n_clusters=k, max_iter=1000).fit(dimension_reduce)
+        colors = ["#%06X" % randint(0, 0xFFFFFF) for i in range(self.k)]
+        kmeans = KMeans(n_clusters=self.k, max_iter=1000).fit(dimension_reduce)
         labels = kmeans.labels_
         centroids = kmeans.cluster_centers_
-        if plot: self.plot(dimension_reduce, colors, labels, centroids)
+        if self.plot: self.plot_graph(dimension_reduce, colors, labels, centroids)
+        return kmeans, labels, centroids, colors
 
-    def plot(self, dimensions, colors, labels, centroids):
+    def results(self):
+        cluster_results = np.zeros([self.k, self.categories.shape[0]])
+        user_skupina = {i: set() for i in range(self.k)}
+        users = self.df["UserID"].unique()
+
+
+        print(self.labels)
+
+        for i,x in enumerate(users):
+            poizvedba = self.original_df.loc[self.original_df["UserID"] == x]
+            categories = poizvedba['SiteCategory'].value_counts()
+
+            print(x, categories)
+            break
+
+
+    @staticmethod
+    def plot_graph(dimensions, colors, labels, centroids):
         print("plot")
         for c, x in zip(labels, dimensions):
             plt.plot(x[0], x[1], ".", color=colors[c], markersize=10.0)
@@ -71,6 +89,6 @@ class ModelUsersWithNoClicks:
         print(self.matrix)
 
 
-a = ModelUsersWithNoClicks(PATH)
-a.kMeans(5, True)
+a = ModelUsersWithNoClicks(PATH, 5, False)
+
 
